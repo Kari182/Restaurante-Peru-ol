397d7927d4251d632068f573031517c6e542b90e
Kari182 <86632449+Kari182@users.noreply.github.com>
2021-10-28 02:01:31 +0200

    agregando imagenes

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,5 +1,6 @@
 class Plate < ApplicationRecord
   belongs_to :category
+  has_one_attached :image
 
   validates :name, :price, :category, presence: true
   validates :description, length: { maximum: 210 }
