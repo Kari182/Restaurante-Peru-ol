7314691d6667b35015598adfe28015ff9b90c552
Kari182 <86632449+Kari182@users.noreply.github.com>
2021-10-31 16:56:56 +0100

    mensaje estilos a home

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get 'pages/carta'
   get 'pages/contacto'
   get 'pages/admin'
+  get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
